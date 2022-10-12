6d7af3d2fee6e697d42708f3276fcb4b82e8f77f
yongin0814 <yongin0814@gmail.com>
2022-10-13 07:05:43 +0900

    1013_과제두개_첫commit

---
+++ src/Ex2.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.util.ArrayList;
+
+public class Ex2 {
+	
+	static int howMany=0;
+
+	public static void main(String[] args) 
+	{
+		// 시작
+		System.out.println("*** Finding Prime Number ***");
+		System.out.println("We will print all the prime numbers between X and Y");
+		
+		// 각종 변수 정의
+		int x,y,randomNum,primeNum;
+		boolean isPrime;
+		Scanner sc = new Scanner(System.in);
+
+		
+		
+		// 값 받기
+		System.out.print("Enter X value(Positive Integer):");
+		x = sc.nextInt();
+		
+		while(x<=1) 
+		{	System.out.print("Enter X value(Positive Integer):");
+			x = sc.nextInt();
+		}
+		
+		System.out.print("Enter Y value(Positive Integer):");
+		y = sc.nextInt();
+		
+		while(y<=1 || y<=x) 
+		{	System.out.print("Enter Y value(Positive Integer):");
+			y = sc.nextInt();
+		}
+		
+		//int[] primeList = new int[y-x+1];
+
+		
+		for ( int i = x ; i <= y ; i++)
+		{
+			
+			is_prime(i);
+			
+			
+		}
+		if( howMany == 0 )
+		{
+			System.out.print("No Prime Number");
+		}
+			
+	}
+	
+	
+
+	public static void is_prime(int number) 
+	{
+		 		
+		// 위 반복문에서 
+		for(int i = 2; i < number; i++) 
+		{        
+
+			// 약수를 갖고 있으면 소수가 아니다
+			if(number % i == 0) 
+			{
+				return ;
+			}
+		}
+		// 약수를 갖고 있지 않으면 소수다
+		System.out.print(number + " ");
+		howMany++;
+		return;
+	}
+	
+
+}
+	
+
+
---
+++ src/Ex3.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+
+public class Ex3 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		// 변수 생성
+		Scanner sc = new Scanner(System.in);
+		int answer,x,y,type,question=0,i=1;
+		int correct=0;
+		float correctRate;
+		
+		// 시작
+		System.out.println("*** Plus Calculation Test ***");
+		System.out.println("You have to enter 0 or minus value to exit");
+		
+		
+		do
+		{
+			// 난수 두개 및 답생성
+			x =	(int)(Math.random()*(99-10+1))+10;
+			y =	(int)(Math.random()*(99-10+1))+10;
+			answer = x+y;
+			question ++; // 문제 낸 수 			
+		
+			
+			// 문제내고 답 받기
+			System.out.print(i+":"+x+"+"+y+"=" );
+			type = sc.nextInt();
+			
+			//맞으면 하나 올려주기 
+			if(type == answer) correct++;
+			i++;
+			
+		} 
+		while(type>0);
+		
+		question = question -1;
+		System.out.println("Your score : " + correct + "/" + question);
+		
+		correctRate = answer/question;		
+		System.out.println("Correct Answer Rate :" +correctRate+"%");
+
+		
+		
+
+	}
+	
+
+	
+
+}
